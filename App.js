9fb9c8e92a5b50d0843a0a2b9026c3d9bcdd5904
Maxpayne <ferhatadibelli9@gmail.com>
2022-03-06 20:25:39 +0300

    third step

--- App.js
+++ App.js
@@ -1,39 +1,7 @@
+const routes = require("./routes");
 const http = require("http");
-const fs = require("fs");
 
-const server = http.createServer((req, res) => {
-  const method = req.method;
-  if (req.url === "/") {
-    res.write("<html>");
-    res.write("<head><title>Enter Message</title></head>");
-    res.write(
-      "<body><form action='/message' method='POST'><input type='text' name='message'><button type='submit'>Send Input</button></input></form></body>"
-    );
-    res.write("</html>");
-    return res.end();
-  }
-  if (req.url === "/message" && method === "POST") {
-    const body = [];
-    req.on("data", (chunk) => {
-      body.push(chunk);
-    });
-
-    return req.on("end", () => {
-      const parsedBody = Buffer.concat(body).toString();
-      const message = parsedBody.split("=")[1];
-      fs.writeFileSync("message.txt", message);
-      res.statusCode = 302;
-      res.setHeader("location", "/");
-      return res.end();
-    });
-  }
-
-  res.setHeader("Content-Type", "text/html");
-  res.write("<html>");
-  res.write("<head><title>My first Server</title></head>");
-  res.write("<body><h1>This is really great for me </h1></body>");
-  res.write("</html>");
-  res.end();
-});
+const server = http.createServer(routes.handler);
+console.log(routes.text);
 
 server.listen(3000);
