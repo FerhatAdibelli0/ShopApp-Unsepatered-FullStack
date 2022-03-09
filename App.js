978432f5bcae0d5b62d49a2387ec7492fa101e59
Maxpayne <ferhatadibelli9@gmail.com>
2022-03-09 21:37:18 +0300

    express added

--- App.js
+++ App.js
@@ -1,7 +1,23 @@
-const routes = require("./routes");
-const http = require("http");
 
-const server = http.createServer(routes.handler);
-console.log(routes.text);
+const express = require("express");
+const app = express();
 
-server.listen(3000);
+app.use("/", (req, res, next) => {
+  console.log("starting");
+  next();
+});
+
+app.use("/add-product", (req, res, next) => {
+  console.log("This is product");
+  res.send("<h1>Adding product item</h1>");
+});
+
+app.use("/", (req, res, next) => {
+  console.log("This is main");
+  res.send("<h1>Welcome Express Js!!</h1>");
+});
+
+app.listen(3000);
+
+// const server = http.createServer(app);
+// server.listen(3000);
