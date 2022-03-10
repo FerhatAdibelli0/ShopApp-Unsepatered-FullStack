63b72996acc2ecfb9e1dceab01e359fc24950b00
Maxpayne <ferhatadibelli9@gmail.com>
2022-03-10 22:29:51 +0300

    not found added

--- App.js
+++ App.js
@@ -9,6 +9,10 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(adminRoutes);
 app.use(shopRoutes);
 
+app.use("/", (req, res, next) => {
+  res.status(404).send("<h1>Not Found Page<h1>");
+});
+
 app.listen(3000);
 
 // const server = http.createServer(app);
