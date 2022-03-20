be02269f888fd53c9ff94a25c832fb1e3c49a983
Maxpayne <ferhatadibelli9@gmail.com>
2022-03-20 21:19:19 +0300

    form and data str changed

--- controller/admin.js
+++ controller/admin.js
@@ -1,7 +1,12 @@
 const Product = require("../models/product");
 
 exports.postProduct = (req, res, next) => {
-  const product = new Product(req.body.title);
+  const title = req.body.title;
+  const imageUrl = req.body.imageUrl;
+  const description = req.body.description;
+  const price = req.body.price;
+
+  const product = new Product(title, imageUrl, description, price);
   product.save();
   res.redirect("/");
 };
