2123099e468ce0d3f2b45beb54be433dac152b07
Maxpayne <ferhatadibelli9@gmail.com>
2022-03-23 23:59:50 +0300

    changed

--- controllers/admin.js
+++ controllers/admin.js
@@ -4,6 +4,7 @@ exports.getAddProduct = (req, res, next) => {
   res.render("admin/edit-product", {
     pageTitle: "Add Product",
     path: "/admin/add-product",
+    editing:false
   });
 };
 
@@ -12,10 +13,14 @@ exports.getEditProduct = (req, res, next) => {
   if (!editMode) {
     return res.redirect("/");
   }
-  res.render("admin/edit-product", {
-    pageTitle: "Edit Product",
-    path: "/admin/edit-product",
-    editting: editMode,
+  const paramId = req.params.productId;
+  Product.findbyId(paramId, (product) => {
+    res.render("admin/edit-product", {
+      pageTitle: "Edit Product",
+      path: "/admin/edit-product",
+      editing: editMode,
+      product: product,
+    });
   });
 };
 
