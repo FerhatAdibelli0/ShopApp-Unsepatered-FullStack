dfafa04e4c6a57b67a7cabad4cb4fd5d04c81ae3
Maxpayne <ferhatadibelli9@gmail.com>
2022-03-31 00:48:30 +0300

    cart quantity adjusted

--- controllers/shop.js
+++ controllers/shop.js
@@ -59,7 +59,7 @@ exports.getIndex = (req, res, next) => {
 };
 
 exports.getCart = (req, res, next) => {
-  console.log(req.user.cart)
+  console.log(req.user.cart);
   req.user
     .getCart()
     .then((cart) => {
@@ -111,10 +111,46 @@ exports.deleteCartProduct = (req, res, next) => {
 
 exports.postCart = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.findbyId(prodId, (product) => {
-    Cart.addToCart(prodId, product.price);
-  });
-  res.redirect("/");
+  let fetchedCart;
+  let newQunatity = 1;
+  req.user
+    .getCart()
+    .then((cart) => {
+      fetchedCart = cart;
+      return cart
+        .getProducts({ where: { id: prodId } })
+        .then((products) => {
+          let product;
+          if (products.length > 0) {
+            product = products[0];
+          }
+          if (product) {
+            let oldQuantity = product.cartItem.quantity;
+            newQunatity = oldQuantity + 1;
+            return product;
+          }
+          return Product.findByPk(prodId);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    })
+    .then((product) => {
+      return fetchedCart.addProduct(product, {
+        through: { quantity: newQunatity },
+      });
+    })
+    .then(() => {
+      res.redirect("/cart");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+
+  // Product.findbyId(prodId, (product) => {
+  //   Cart.addToCart(prodId, product.price);
+  // });
+  // res.redirect("/");
 };
 
 exports.getOrders = (req, res, next) => {
