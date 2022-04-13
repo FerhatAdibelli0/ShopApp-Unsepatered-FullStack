40130598cd43a530fb3d2d3c57dfcdbd1d7ea2a7
Maxpayne <ferhatadibelli9@gmail.com>
2022-04-13 23:00:12 +0300

    changes arranged for auth

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ const bodyParser = require("body-parser");
 const errorController = require("./controllers/error");
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
+const authRoutes = require("./routes/auth");
 // const mongoConnect = require("./util/database").mongoConnect;
 const User = require("./models/users");
 const mongoose = require("mongoose");
@@ -41,6 +42,7 @@ app.use((req, res, next) => {
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
+app.use(authRoutes);
 app.use(errorController.get404);
 
 //Associations for MySQL
